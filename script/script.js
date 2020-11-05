42b6210282cd710dcc2faa5c54d5769e4fbc8777
Priluchnaya Olga <ob261264@gmail.com>
2020-11-05 22:23:52 +0300

    первая обработка табов

--- script/script.js
+++ script/script.js
@@ -147,8 +147,49 @@ window.addEventListener('DOMContentLoaded', function() {
 
     togglePopup();
 
+    //табы
+    const tabs = () => {
+        const tabHeader = document.querySelector('.service-header'),
+            tab = tabHeader.querySelectorAll('.service-header-tab'),
+            tabContent = document.querySelectorAll('.service-tab');
 
+        const toggleTabContent = (index) => {
+            for (let i = 0; i < tabContent.length; i++) {
+                if (index === i) {
+                    tab[i].classList.add('active');
+                    tabContent[i].classList.remove('d-none');
+                } else {
+                    tab[i].classList.remove('active');
+                    tabContent[i].classList.add('d-none');
+                };
+            };
+        };
 
+        tabHeader.addEventListener('click', (event) => {
+            let target = event.target;
+            //console.log(target);
+            while (target !== tabHeader) {
+                // console.log(target);
 
+                if (target.classList.contains('service-header-tab')) {
+                    //console.log(target);
+                    tab.forEach((item, i) => {
+                        if (item === target) {
+                            // console.log(tabContent[i]);
+                            toggleTabContent(i);
+                        };
+                    });
+
+                    return;
+                };
+
+                target = target.parentNode;
+
+            };
+
+        });
+    };
+
+    tabs();
 
 });
