78e93f3aef5f5fedb63fe9b878d634b1fd4afc74
Priluchnaya Olga <ob261264@gmail.com>
2020-11-10 22:48:25 +0300

    калькулятор

--- script/script.js
+++ script/script.js
@@ -352,7 +352,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     const classRow = () => {
         const commandPhoto = document.querySelectorAll('.command__photo');
-        console.log(commandPhoto);
+        // console.log(commandPhoto);
         commandPhoto.forEach((img) => {
             let src = img.getAttribute('src');
             img.addEventListener('mouseenter', (e) => {
@@ -370,4 +370,62 @@ window.addEventListener('DOMContentLoaded', function() {
     };
     classRow();
 
+    //калькулятор
+    const calc = (price) => {
+        const classBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'), //тип помещения
+            calcSquare = document.querySelector('.calc-square'), //площадь
+            calcСount = document.querySelector('.calc-count'), //количество помещений
+            calcDay = document.querySelector('.calc-day'), //срок исполнения
+            totalValue = document.getElementById('total');
+
+        const countSum = () => {
+            let total = 0,
+                countValue = 1,
+                dayValue = 1;
+
+            const typeValue = calcType.options[calcType.selectedIndex].value, // selected тип объекта
+                squareValue = +calcSquare.value; //площадь
+
+            if (calcСount.value > 1) {
+                countValue += (calcСount.value - 1) / 10;
+            };
+
+            if (calcDay.value && calcDay.value < 5) {
+                dayValue *= 2;
+            } else if (calcDay.value && calcDay.value < 10) {
+                dayValue *= 1.5;
+            };
+
+            // console.log(!!typeValue);
+            // console.log(!!squareValue);
+            if (typeValue && squareValue) {
+                total = Math.round(price * typeValue * squareValue * countValue * dayValue);
+            };
+
+            totalValue.textContent = total;
+
+        };
+
+        classBlock.addEventListener('change', (event) => {
+            const target = event.target;
+
+            // if (target.matches('.calc-type') || target.matches('.calc-square') ||
+            //     target.matches('.calc-count') || target.matches('.calc-day')) {
+            //     console.log(1);
+            // }; //первый способ
+
+            // if (target === calcType || target === calcSquare || target === calcСount || target === calcDay) {
+            //     console.log(1);
+            // }; //второй способ
+
+            if (target.matches('select') || target.matches('input')) {
+                //console.log(1);
+                countSum(); //функция считает итоговую цену
+            }; //третий способ
+
+        });
+
+    };
+    calc(100);
 });
