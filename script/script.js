c88ef49225ee6a1c089a181a39e755911b0a07fb
Priluchnaya Olga <ob261264@gmail.com>
2020-11-17 23:59:22 +0300

    fatch-formData

--- script/script.js
+++ script/script.js
@@ -429,7 +429,6 @@ window.addEventListener('DOMContentLoaded', function() {
     };
     calc(100);
 
-
     //sends-ajax-forms-fetch
     const sendForm = () => {
 
@@ -456,20 +455,20 @@ window.addEventListener('DOMContentLoaded', function() {
                 statusMessage.textContent = loadMessage;
                 //получение данных с формы в обработчике событий
                 const formData = new FormData(elem);
-                let body = {};
-                // for (let val of formData.entries()) {
-                //     // console.log(val);
-                //     body[val[0]] = val[1];
-                // };
-                formData.forEach((val, key) => {
-                    body[key] = val;
-                });
+                /* let body = {};
+                 // for (let val of formData.entries()) {
+                 //     // console.log(val);
+                 //     body[val[0]] = val[1];
+                 // };
+                 formData.forEach((val, key) => {
+                     body[key] = val;
+                 });*/
 
 
 
                 //console.log(body);
                 //передача данных при вызове функции
-                postData(body)
+                postData(formData)
                     .then((response) => {
                         if (response.status !== 200) {
                             throw new Error('status network not 200');
@@ -490,14 +489,14 @@ window.addEventListener('DOMContentLoaded', function() {
 
         //запрос на сервер в отдельной функции postData()
         //outputData -callback функция - данные, которые вернулись, обрабатываются
-        const postData = (body) => {
+        const postData = (formData) => {
 
             return fetch('./server.php', {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/json'
+                    'Content-Type': 'multipart/form-data'
                 },
-                body: JSON.stringify(body),
+                body: formData,
                 credentials: 'include',
                 cache: 'default'
 
@@ -538,6 +537,115 @@ window.addEventListener('DOMContentLoaded', function() {
     };
 
     sendForm();
+
+    //sends-ajax-forms-fetch
+    // const sendForm = () => {
+
+    //     const form = document.querySelectorAll('form');
+
+    //     // console.log(form);
+
+    //     // const form = document.getElementById('form1');
+
+    //     //statusMessage.textContent = 'Тут будет сообщение!'
+
+    //     form.forEach((elem) => {
+    //         const errorMessage = 'Что-то пошло не так...',
+    //             loadMessage = 'Загрузка...',
+    //             successMessage = 'Спасибо! Мы скоро с Вами свяжемся!';
+
+    //         elem.addEventListener('submit', (event) => {
+    //             const statusMessage = document.createElement('div');
+    //             statusMessage.style.cssText = 'font-size: 2rem; color: #fff;';
+
+
+    //             event.preventDefault();
+    //             elem.appendChild(statusMessage);
+    //             statusMessage.textContent = loadMessage;
+    //             //получение данных с формы в обработчике событий
+    //             const formData = new FormData(elem);
+    //             let body = {};
+    //             // for (let val of formData.entries()) {
+    //             //     // console.log(val);
+    //             //     body[val[0]] = val[1];
+    //             // };
+    //             formData.forEach((val, key) => {
+    //                 body[key] = val;
+    //             });
+
+
+
+    //             //console.log(body);
+    //             //передача данных при вызове функции
+    //             postData(body)
+    //                 .then((response) => {
+    //                     if (response.status !== 200) {
+    //                         throw new Error('status network not 200');
+    //                     };
+    //                     console.log(response);
+    //                     elem.reset();
+    //                     statusMessage.textContent = successMessage;
+    //                     setTimeout(() => statusMessage.remove(), 7000);
+    //                 })
+    //                 .catch((error) => {
+    //                     statusMessage.textContent = errorMessage;
+    //                     console.error(error);
+    //                 });
+    //         });
+
+
+    //     });
+
+    //     //запрос на сервер в отдельной функции postData()
+    //     //outputData -callback функция - данные, которые вернулись, обрабатываются
+    //     const postData = (body) => {
+
+    //         return fetch('./server.php', {
+    //             method: 'POST',
+    //             headers: {
+    //                 'Content-Type': 'application/json'
+    //             },
+    //             body: JSON.stringify(body),
+    //             credentials: 'include',
+    //             cache: 'default'
+
+    //         });
+
+    //         /* return new Promise((resolve, reject) => {
+    //              const request = new XMLHttpRequest();
+
+    //              request.addEventListener('readystatechange', () => {
+    //                  // statusMessage.textContent = loadMessage;
+
+    //                  if (request.readyState !== 4) {
+    //                  return;
+    //                  };
+
+    //                  if (request.status === 200) {
+    //                      resolve();
+    //                      // statusMessage.textContent = successMessage;
+    //                  } else {
+    //                      reject(request.status);
+    //                      // statusMessage.textContent = errorMessage;
+    //                      // console.error(request.status);
+    //                  };
+
+    //              });
+
+    //              request.open('POST', './server.php');
+    //              //request.setRequestHeader('Content-Type', 'multipart/form-data');
+    //              request.setRequestHeader('Content-Type', 'application/json');
+
+    //              //request.send(formData);
+    //              request.send(JSON.stringify(body));
+
+    //          });*/
+
+    //     };
+
+    // };
+
+    // sendForm();
 
 
 
